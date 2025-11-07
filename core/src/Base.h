8857a56ada3b02b4aaf7b52077084cc3c00bed6a
Spawn <spwn02.dev@gmail.com>
2025-11-07 10:36:42 +0100

    Fixed logger & added logs to gitignore.

--- core/src/Base.h
+++ core/src/Base.h
@@ -19,6 +19,3 @@
 #define BIT(x) (1 << x)
 
 #define CORE_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
-
-#include "Log.h"
-#include "Assert.h"
